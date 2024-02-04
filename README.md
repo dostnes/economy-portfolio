9e7a9b4d4993fcc70dd4a6453b868e3fc9690171
Daniel Ostnes <daniel.ostnes16@gmail.com>
2024-02-04 17:05:23 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Coingecko API to Google Sheets
+#### Updated 03.02.2024
+
+
+## Requirements
+
++ Create a new Google Sheet / navigate to existing Google Sheet
++ Go to Google Sheets API: https://developers.google.com/sheets/api/guides/concepts
++ Quickstart for Python here: https://developers.google.com/sheets/api/quickstart/python
+    * Before using Google APIs, you need to turn them on in a GC project. Click "Enable the API" as seen in the quickstart guide.
+    * If no projects exist, then create one.
+    * Navigate to section "Authorize credentials for a desktop application" and click "Go to Credentials".
+    * Click + Create Credentials
+    * Create new OAuth client ID
+    * Choose "Web application" as Application type.
+    * Give the application a name ("My Python Scripts")
+    * Authorized redirect URIs: http://localhost:3000/
+    * When the client is created, download JSON file
++ Inspiration video: https://www.youtube.com/watch?v=X-L1NKoEi10
+
+
+### The use of localhost:3000
+
+The usage of http://localhost:3000/ in your project is related to the OAuth 2.0 authorization flow, specifically the web server flow (also known as the "Authorization Code" flow). In this flow, your application redirects the user to the Google Authorization Server for authentication, and Google redirects the user back to your application with an authorization code.
+
+Here's a breakdown of the process:
+
+User Authentication: When your application needs to access a user's Google Sheets, it redirects the user to the Google Accounts login page. The user logs in and grants permission to your application.
+
+Authorization Code: After the user grants permission, Google redirects the user back to your application's specified redirect URI (http://localhost:3000/ in your case) with an authorization code.
+
+Token Exchange: Your application then exchanges this authorization code for an access token and a refresh token by making a request to the Google Authorization Server.
+
+Access Google APIs: With the obtained access token, your application can make requests to Google Sheets API on behalf of the user.
+
+The reason you had to register http://localhost:3000/ in the Google Cloud Platform when creating credentials is to specify a valid and secure redirect URI for your application. The redirect URI is a critical part of the OAuth 2.0 flow to ensure that the authorization code is sent back to the correct endpoint.
+
+In a production environment, you would typically use a publicly accessible URL (e.g., the URL where your application is deployed), but during development, it's common to use localhost for testing purposes.
+
+To sum up, http://localhost:3000/ is the redirect URI where Google sends the user after they authenticate and grant permission, allowing your application to complete the OAuth 2.0 flow and obtain the necessary tokens to access Google Sheets on behalf of the user.
---
+++ gsheet.py
@@ -0,0 +1,113 @@
+# Base code from https://developers.google.com/sheets/api/quickstart/python
+
+import os.path
+import requests
+import time
+from datetime import datetime
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+# The ID and range of the spreadsheet.
+SAMPLE_SPREADSHEET_ID = "1J-NvxzITGiWdvbSMk_0OcusLIOVvg1LnrScpjf5rXik"
+SAMPLE_RANGE_NAME = "coingecko_data!A2"
+
+# Function that calls Coingecko API
+# Paginates through given range
+# Calculates API runtime and status code
+# Appends timestamp from last runtime 
+
+def getCryptoAssets():
+    base_url = "https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        'vs_currency': 'nok',
+        'per_page': 250,
+        'page': None 
+    }
+    myValues = []
+
+    for page in range (1, 3): # This range can be adjusted based on how many pages you want to fetch
+        params['page'] = page
+
+        # Record the start time of the API request
+
+        start_time = time.time()
+
+        try:
+
+            response = requests.get(base_url, headers={}, params=params)
+            response.raise_for_status() # Raise an HTTPError for bad responses (4xx or 5xx)
+
+            data = response.json()
+
+            # Calculate the time taken for the API request
+            elapsed_time = time.time() - start_time
+
+            # Print information about the API request
+            print(f"API Request - Page {page}")
+            print(f"URL: {response.url}")
+            print(f"Status Code: {response.status_code}")
+            print(f"Time Taken: {elapsed_time:.2f} seconds")
+
+            for item in data:
+                myValues.append([
+                    item['id'],
+                    item['symbol'],
+                    item['name'],
+                    item['current_price'],
+                    item['image'],
+                    item['market_cap_rank']
+                ])
+
+        except requests.exceptions.RequestException as e:
+            # Handle exceptions here
+            print(f"Error in API request - Page {page} {e}")
+
+    # Append the timestamp to the data
+    timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    myValues.append([f"Last Updated:"])
+    myValues.append([timestamp])
+
+    return myValues
+
+
+def main():
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=3000)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("sheets", "v4", credentials=creds)
+
+    # Call the Sheets API
+    valueData = getCryptoAssets()
+    sheet = service.spreadsheets()
+    result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                   range=SAMPLE_RANGE_NAME, valueInputOption="USER_ENTERED", body={"values": valueData}).execute()
+
+  except HttpError as err:
+    print(err)
+
+
+if __name__ == "__main__":
+  main()
